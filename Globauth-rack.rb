cb60dfa73576561470fa65fc31faab449a188f9f
Matthieu Riviere <poldenton@leukensis.org>
2010-08-29 08:40:00 +0000

    Initial rack-middleware adapter

---
+++ Globauth-rack.rb
@@ -0,0 +1,53 @@
+require_relative 'lib/db'
+
+class Globauth
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+
+    # Setup everything so we have access to env and params easily
+    @env = env
+	puts "Env : #{@env}"
+	@request = Rack::Request.new(env)
+	puts "Request : #{@request}"
+	@params = @request.params
+	puts "Params : #{@params}"
+	
+	# Three possible cases are possible :
+	# - We are already authed. Check cookie/session
+	puts "Session id : #{@env['rack.session']['uid']}"
+	if @env['rack.session']['uid'] && @env['rack.session']['user'] && @env['rack.session']['groups'] then
+		puts "Already authed user !"
+	
+	# - We are authing. Check POST method + login/pass in params
+	elsif @env['REQUEST_METHOD'] = "POST" && @params['wUser'] && @params['wPass'] then
+		puts "User #{@params['wUser']} is trying to authenticate with pass : #{@params['wPass']}"
+		p = Profile.get_user(@params['wUser'],@params['wPass'])
+		if p then
+			puts 'Authing was successful !'
+			@env['rack.session']['uid'] = p.id
+			@env['rack.session']['user'] = p.user
+			@env['rack.session']['groups'] = Hash.new
+			p.groups.each do |g|
+				@env['rack.session']['groups'][g.name] = g.descr
+			end
+		end
+
+	# - Else, we are unauthed
+	else
+		@env['rack.session'] = Hash.new
+	end
+	
+	# If everything is all right, we call the app
+	status, headers, response = @app.call(env)
+	
+	# We then need to tweak response to add authentication-related boxes on the final page
+	puts "Status #{status}"
+	puts "Headers #{headers}"
+	puts "Response #{response}"
+    
+	[status, headers, ["Lol" << response.first]]
+  end
+end
---
+++ config.ru
@@ -0,0 +1,20 @@
+require_relative 'Globauth-rack'
+
+use Rack::Lint
+use Rack::ContentLength
+use Rack::ShowExceptions
+
+use Rack::Session::Pool, :domain => '82.146.49.253', :expire_after => 60 * 60 * 24 * 30, :secure => true
+use Globauth
+
+app = lambda { |env| 
+	if env['rack.session']['uid'] then
+		greet_msg = "Hello #{env['rack.session']['user']} (#{env['rack.session']['uid']}) (#{env['rack.session']['groups']}) <br />"
+	else
+		greet_msg = ''
+	end
+	[200, { 'Content-Type' => 'text/html' }, ['Hello World!<br />
+		' << greet_msg << '
+		<form method="post" action=""><input type="text" name="wUser"/><input type="text" name="wPass"/><input type="submit" /></form>'] ]}
+run app
+
