e49ec4ef776da9a95da7fb81a6048c3a467ab4eb
Matthieu Riviere <poldenton@leukensis.org>
2010-08-30 11:52:05 +0000

    * Use env instead of @env * Bugfix ( == instead of = ) * Make div substitution haml-compliant (use ' instead of ")

--- Globauth-rack.rb
+++ Globauth-rack.rb
@@ -8,32 +8,31 @@ class Globauth
   def call(env)
 
     # Setup everything so we have access to env and params easily
-    @env = env
 	@request = Rack::Request.new(env)
 	@params = @request.params
 	
 	# Three possible cases are possible :
 	# - We are already authed. Check cookie/session
-	if @env['rack.session']['uid'] && @env['rack.session']['user'] && @env['rack.session']['groups'] then
+	if env['rack.session']['uid'] && env['rack.session']['user'] && env['rack.session']['groups'] then
 		# Nothing to do ?
 	
 	# - We are authing. Check POST method + login/pass in params
-	elsif @env['REQUEST_METHOD'] = "POST" && @params['wUser'] && @params['wPass'] then
+	elsif env['REQUEST_METHOD'] == "POST" && @params['wUser'] && @params['wPass'] then
 		p = Profile.get_user(@params['wUser'],@params['wPass'])
 		if p then
-			@env['rack.session']['uid'] = p.id
-			@env['rack.session']['user'] = p.user
-			@env['rack.session']['groups'] = Hash.new
+			env['rack.session']['uid'] = p.id
+			env['rack.session']['user'] = p.user
+			env['rack.session']['groups'] = Hash.new
 			p.groups.each do |g|
-				@env['rack.session']['groups'][g.name] = g.descr
+				env['rack.session']['groups'][g.name] = g.descr
 			end
 		end
 
 	# - Else, we are unauthed
 	else
-		@env['rack.session'] = Hash.new
+		env['rack.session'] = Hash.new
 	end
-	
+
 	# If everything is all right, we call the app
 	status, headers, response = @app.call(env)
 	
@@ -41,15 +40,15 @@ class Globauth
 	# We then need to tweak response to add authentication-related boxes on the final page
 
 	# If authed, we want to know as who
-	if @env['rack.session']['uid'] && @env['rack.session']['user'] && @env['rack.session']['groups'] then
-		lgbox="Authed as #{@env['rack.session']['user']}"
+	if env['rack.session']['uid'] && env['rack.session']['user'] && env['rack.session']['groups'] then
+		lgbox="Authed as #{env['rack.session']['user']}"
 
 	# Else, we want a way to authenticate ourselves
 	else
 		lgbox="<form method=\"post\" action=\"\"><input type=\"text\" content=\"User\" name=\"wUser\"/><input type=\"text\" content=\"Pass\" name=\"wPass\"/><input type=\"submit\" /></form>"
 	end
 
-	puts response.first.sub!('<div id="loginbox">','<div id="loginbox">' << lgbox)
+	puts response.first.sub!('<div id=\'loginbox\'>','<div id=\'loginbox\'>' << lgbox)
     
 	[status, headers, response]
   end
